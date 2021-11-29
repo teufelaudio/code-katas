6df73b3b8ada0cb51e27e278bb527a292f28ab53
Jose Maria Valera Reales <jose.valera@teufel.de>
2021-11-29 10:02:00 +0100

    Add roman-numerals

---
+++ tdd/roman-numerals/src/RomanNumerals.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Kata;
+
+final class RomanNumerals
+{
+    public function changeMe()
+    {
+        return true;
+    }
+}
---
+++ tdd/roman-numerals/tests/Unit/RomanNumeralsTest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace KataTests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class RomanNumeralsTest extends TestCase
+{
+}
