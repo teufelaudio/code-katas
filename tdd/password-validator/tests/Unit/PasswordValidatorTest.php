b5780a517e4d04ef5d96262bb3cea791a4e9c1f6
Florian Barndt <florian.barndt@teufel.de>
2021-12-01 15:21:09 +0100

    refactor test to assert true for valid passwords

--- tdd/password-validator/tests/Unit/PasswordValidatorTest.php
+++ tdd/password-validator/tests/Unit/PasswordValidatorTest.php
@@ -14,17 +14,17 @@ final class PasswordValidatorTest extends TestCase
         self::assertFalse($validator->validatePassword('abc'));
     }
 
-    public function test_return_true_on_length_greater_or_equal_8(): void
-    {
-        $validator = new PasswordValidator();
-
-        self::assertTrue($validator->validatePassword('Abcdefgh'));
-    }
-
     public function test_return_false_on_missing_uppercase_letter(): void
     {
         $validator = new PasswordValidator();
 
         self::assertFalse($validator->validatePassword('abcdefgh'));
+    }
+
+    public function test_return_true_on_valid_password(): void
+    {
+        $validator = new PasswordValidator();
+
+        self::assertTrue($validator->validatePassword('aBcdefgh'));
     }
 }
