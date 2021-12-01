1172cd6055ff37e57a2bccba15a5850c34160b5a
Jose Maria Valera Reales <jose.valera@teufel.de>
2021-12-01 15:35:16 +0100

    Refactoring PasswordValidatorTest

--- tdd/password-validator/tests/Unit/PasswordValidatorTest.php
+++ tdd/password-validator/tests/Unit/PasswordValidatorTest.php
@@ -7,45 +7,40 @@ use PHPUnit\Framework\TestCase;
 
 final class PasswordValidatorTest extends TestCase
 {
+    private PasswordValidator $validator;
+
+    protected function setUp(): void
+    {
+        $this->validator = new PasswordValidator();
+    }
+
     public function test_fail_on_length_less_than_8(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validatePassword('abc'));
+        self::assertFalse($this->validator->validatePassword('abc'));
     }
 
     public function test_fail_on_missing_uppercase_letter(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validatePassword('abcdefgh'));
+        self::assertFalse($this->validator->validatePassword('abcdefgh'));
     }
 
     public function test_fail_on_missing_lowercase_letter(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validatePassword('AAAAAAAA'));
+        self::assertFalse($this->validator->validatePassword('AAAAAAAA'));
     }
 
     public function test_fail_on_missing_digit(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validatePassword('aBcdefgh'));
+        self::assertFalse($this->validator->validatePassword('aBcdefgh'));
     }
 
     public function test_fail_on_missing_underscore(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertFalse($validator->validatePassword('aBcdefg1'));
+        self::assertFalse($this->validator->validatePassword('aBcdefg1'));
     }
 
-    public function test_return_true_on_valid_password(): void
+    public function test_valid_password(): void
     {
-        $validator = new PasswordValidator();
-
-        self::assertTrue($validator->validatePassword('aB3_efgh'));
+        self::assertTrue($this->validator->validatePassword('aB3_efgh'));
     }
 }
