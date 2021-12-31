e77f840f2f8cf5ea5f593800cbf5e832d55c0cc0
TeufelSL <sebastian.larisch@teufel.de>
2021-12-31 10:48:24 +0100

    let fizzbuzz return numbers 1,2

--- tdd/fizz-buzz/tests/Unit/FizzBuzzTest.php
+++ tdd/fizz-buzz/tests/Unit/FizzBuzzTest.php
@@ -7,12 +7,31 @@ use PHPUnit\Framework\TestCase;
 
 final class FizzBuzzTest extends TestCase
 {
+    private FizzBuzz $fizzBuzz;
+
+    protected function setUp(): void
+    {
+        $this->fizzBuzz = new FizzBuzz();
+    }
+
     public function test_ensure_the_list_has_100_values(): void
     {
-        $fizzBuzz = new FizzBuzz();
-
-        $returnValues = $fizzBuzz->fizzBuzz();
+        $returnValues = $this->fizzBuzz->fizzBuzz();
 
         self::assertCount(100, $returnValues);
+    }
+
+    public function test_ensure_return_number_1_for_first_number(): void
+    {
+        $returnValues = $this->fizzBuzz->fizzBuzz();
+
+        self::assertEquals(1, $returnValues[0]);
+    }
+
+    public function test_ensure_return_number_2_for_second_number(): void
+    {
+        $returnValues = $this->fizzBuzz->fizzBuzz();
+
+        self::assertEquals(2, $returnValues[1]);
     }
 }
