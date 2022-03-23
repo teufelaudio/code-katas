e2503e13be3480a1960a07757372d6c6fa7f935a
Florian Barndt <florian.barndt@teufel.de>
2022-03-23 14:53:45 +0100

    formatting

--- tdd/leap-year/javascript/src/kata.js
+++ tdd/leap-year/javascript/src/kata.js
@@ -1,11 +1,11 @@
 const isLeapYear = (year) => {
-    if(year % 400 === 0) return true;
+  if (year % 400 === 0) return true;
 
-    if(year % 100 === 0) return false;
+  if (year % 100 === 0) return false;
 
-    if(year % 4 === 0) return true;
+  if (year % 4 === 0) return true;
 
-    return false;
+  return false;
 };
 
-module.exports = {isLeapYear};
+module.exports = { isLeapYear };
--- tdd/leap-year/javascript/test/kata.test.js
+++ tdd/leap-year/javascript/test/kata.test.js
@@ -1,27 +1,27 @@
-const {isLeapYear} = require('../src/kata');
+const { isLeapYear } = require("../src/kata");
 
 describe("Leap Year Kata", function () {
-    it("is a leap year if it's divisible by 400", function () {
-        expect(isLeapYear(0)).toBe(true);
-        expect(isLeapYear(1)).toBe(false);
-        expect(isLeapYear(400)).toBe(true);
-        expect(isLeapYear(2000)).toBe(true);
-    });
+  it("is a leap year if it's divisible by 400", function () {
+    expect(isLeapYear(0)).toBe(true);
+    expect(isLeapYear(1)).toBe(false);
+    expect(isLeapYear(400)).toBe(true);
+    expect(isLeapYear(2000)).toBe(true);
+  });
 
-    it("is not a leap year if it is divisible by 100 but not by 400", function () {
-        expect(isLeapYear(1600)).toBe(true);
-        expect(isLeapYear(1700)).toBe(false);
-        expect(isLeapYear(1800)).toBe(false);
-        expect(isLeapYear(1900)).toBe(false);
-        expect(isLeapYear(2000)).toBe(true);
-        expect(isLeapYear(2100)).toBe(false);
-    });
+  it("is not a leap year if it is divisible by 100 but not by 400", function () {
+    expect(isLeapYear(1600)).toBe(true);
+    expect(isLeapYear(1700)).toBe(false);
+    expect(isLeapYear(1800)).toBe(false);
+    expect(isLeapYear(1900)).toBe(false);
+    expect(isLeapYear(2000)).toBe(true);
+    expect(isLeapYear(2100)).toBe(false);
+  });
 
-    it("all years divisible by 4 but not by 100 are leap years", function () {
-        expect(isLeapYear(4)).toBe(true);
-        expect(isLeapYear(8)).toBe(true);
-        expect(isLeapYear(2008)).toBe(true);
-        expect(isLeapYear(2012)).toBe(true);
-        expect(isLeapYear(2016)).toBe(true);
-    });
+  it("all years divisible by 4 but not by 100 are leap years", function () {
+    expect(isLeapYear(4)).toBe(true);
+    expect(isLeapYear(8)).toBe(true);
+    expect(isLeapYear(2008)).toBe(true);
+    expect(isLeapYear(2012)).toBe(true);
+    expect(isLeapYear(2016)).toBe(true);
+  });
 });
