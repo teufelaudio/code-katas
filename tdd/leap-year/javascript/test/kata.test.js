b217141d4ee9282bc002bafe73a6b139794bf6de
Jose Maria Valera Reales <jose.valera@teufel.de>
2022-03-23 14:48:44 +0100

    is not a leap year if it is divisible by 100 but not by 400

--- tdd/leap-year/javascript/test/kata.test.js
+++ tdd/leap-year/javascript/test/kata.test.js
@@ -7,4 +7,13 @@ describe("Leap Year Kata", function () {
         expect(isLeapYear(400)).toBe(true);
         expect(isLeapYear(2000)).toBe(true);
     });
+
+    it("is not a leap year if it is divisible by 100 but not by 400", function () {
+        expect(isLeapYear(1600)).toBe(true);
+        expect(isLeapYear(1700)).toBe(false);
+        expect(isLeapYear(1800)).toBe(false);
+        expect(isLeapYear(1900)).toBe(false);
+        expect(isLeapYear(2000)).toBe(true);
+        expect(isLeapYear(2100)).toBe(false);
+    });
 });
