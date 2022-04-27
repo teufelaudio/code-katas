ee48450883ca93669172706e0f3949ed3d55fefe
Dominik Marx <dominik.marx@teufel.de>
2022-04-27 14:24:53 +0200

    test single product

--- tdd/bundle-configurator/php/tests/BundleConfiguratorTest.php
+++ tdd/bundle-configurator/php/tests/BundleConfiguratorTest.php
@@ -8,10 +8,21 @@ use PHPUnit\Framework\TestCase;
 
 final class BundleConfiguratorTest extends TestCase
 {
-    public function test_change_me(): void
+    public function test_get_one_product(): void
     {
         $bundleConfigurator = new BundleConfigurator();
 
         self::assertSame('P1', $bundleConfigurator->select('P1'));
+        self::assertSame('P2', $bundleConfigurator->select('P2'));
     }
+
+//    public function test_get_one_product(): void
+//    {
+//        $bundleConfigurator = new BundleConfigurator();
+//
+//        self::assertSame('P1', $bundleConfigurator->select('P1'));
+////        self::assertSame('P2', $bundleConfigurator->select('P2'));
+////        self::assertSame('B1', $bundleConfigurator->select('P1,P2'));
+////        self::assertSame('B1', $bundleConfigurator->select('P2,P1'));
+//    }
 }
