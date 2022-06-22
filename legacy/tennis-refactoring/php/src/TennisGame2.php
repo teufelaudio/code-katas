64a40cfd8c2920306d8843c9c081e06c22c768da
TeufelSL <sebastian.larisch@teufel.de>
2022-06-22 15:11:19 +0200

    remove unnessesary if conditions

--- legacy/tennis-refactoring/php/src/TennisGame2.php
+++ legacy/tennis-refactoring/php/src/TennisGame2.php
@@ -60,64 +60,29 @@ class TennisGame2 implements TennisGame
         }
 
         if ($this->player1Points > 0 && $this->player2Points === 0) {
-            if ($this->player1Points === 1) {
-                $this->player1Result = $this->getNumberName(1);
-            }
-            if ($this->player1Points === 2) {
-                $this->player1Result = $this->getNumberName(2);
-            }
-            if ($this->player1Points === 3) {
-                $this->player1Result = $this->getNumberName(3);
-            }
-
+            $this->player1Result = $this->getNumberName($this->player1Points);
             $this->player2Result = $this->getNumberName(0);
             $score = "{$this->player1Result}-{$this->player2Result}";
         }
 
         if ($this->player2Points > 0 && $this->player1Points === 0) {
-            if ($this->player2Points === 1) {
-                $this->player2Result = $this->getNumberName(1);
-            }
-            if ($this->player2Points === 2) {
-                $this->player2Result = $this->getNumberName(2);
-            }
-            if ($this->player2Points === 3) {
-                $this->player2Result = $this->getNumberName(3);
-            }
+            $this->player2Result = $this->getNumberName($this->player2Points);
             $this->player1Result = $this->getNumberName(0);
             $score = "{$this->player1Result}-{$this->player2Result}";
         }
 
 
         if ($this->player2Points > $this->player1Points && $this->player2Points < 4) {
-            if ($this->player2Points === 2) {
-                $this->player2Result = $this->getNumberName(2);
-            }
-            if ($this->player2Points === 3) {
-                $this->player2Result = $this->getNumberName(3);
-            }
-            if ($this->player1Points === 1) {
-                $this->player1Result = $this->getNumberName(1);
-            }
-            if ($this->player1Points === 2) {
-                $this->player1Result = $this->getNumberName(2);
-            }
+            $this->player2Result = $this->getNumberName($this->player2Points);
+            $this->player1Result = $this->getNumberName($this->player1Points);
+
             return "{$this->player1Result}-{$this->player2Result}";
         }
 
         if ($this->player1Points > $this->player2Points && $this->player1Points < 4) {
-            if ($this->player1Points === 2) {
-                $this->player1Result = $this->getNumberName(2);
-            }
-            if ($this->player1Points === 3) {
-                $this->player1Result = $this->getNumberName(3);
-            }
-            if ($this->player2Points === 1) {
-                $this->player2Result = $this->getNumberName(1);
-            }
-            if ($this->player2Points === 2) {
-                $this->player2Result = $this->getNumberName(2);
-            }
+            $this->player1Result = $this->getNumberName($this->player1Points);
+            $this->player2Result = $this->getNumberName($this->player2Points);
+
             return "{$this->player1Result}-{$this->player2Result}";
         }
 
