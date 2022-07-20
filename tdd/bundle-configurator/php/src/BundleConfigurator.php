7b5aebc5a14f15323ae4b21cc3ef4ee8736c96b7
Dominik Marx <dominik.marx@teufel.de>
2022-07-20 14:34:09 +0200

    test_select_product1_and_product2_and_get_back_bundle1

--- tdd/bundle-configurator/php/src/BundleConfigurator.php
+++ tdd/bundle-configurator/php/src/BundleConfigurator.php
@@ -5,8 +5,16 @@ namespace Kata;
 
 final class BundleConfigurator
 {
+    private const BUNDLES = [
+        'B1' => 'P1,P2',
+        'B2' => 'P1,P4',
+        'B3' => 'P3,P4',
+        'B4' => 'P1,P2,P3,P4',
+        'B5' => 'P1,P5',
+    ];
+
     public function select(string $productNames): string
     {
-        return 'P1';
+        return array_search($productNames, self::BUNDLES) ?: $productNames;
     }
 }
--- tdd/bundle-configurator/php/tests/BundleConfiguratorTest.php
+++ tdd/bundle-configurator/php/tests/BundleConfiguratorTest.php
@@ -8,10 +8,17 @@ use PHPUnit\Framework\TestCase;
 
 final class BundleConfiguratorTest extends TestCase
 {
-    public function test_change_me(): void
+    public function test_select_product1_and_get_same_product_back(): void
     {
         $bundleConfigurator = new BundleConfigurator();
 
         self::assertSame('P1', $bundleConfigurator->select('P1'));
+    }
+
+    public function test_select_product1_and_product2_and_get_back_bundle1(): void
+    {
+        $bundleConfigurator = new BundleConfigurator();
+
+        self::assertSame('B1', $bundleConfigurator->select('P1,P2'));
     }
 }
