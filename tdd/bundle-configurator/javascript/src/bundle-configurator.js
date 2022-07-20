66e013315e719f5e14182008ad2e7c9c1e0cffb4
Jose Maria Valera Reales <jose.valera@teufel.de>
2022-07-20 14:34:13 +0200

    Change signature of selectProducts(): list<string>

--- tdd/bundle-configurator/javascript/src/bundle-configurator.js
+++ tdd/bundle-configurator/javascript/src/bundle-configurator.js
@@ -1,7 +1,7 @@
 const selectProducts = (...productNames) => {
-    if (productNames.length === 1) return "P1";
+    if (productNames.length === 1) return ["P1"];
 
-    return "B1";
+    return ["B1"];
 };
 
 module.exports = {selectProducts};
--- tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
+++ tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
@@ -4,18 +4,18 @@ describe("Bundle configurator", function () {
     it("returns the product if there's only one", function () {
         const result = selectProducts("P1");
 
-        expect(result).toBe("P1");
+        expect(result).toStrictEqual(["P1"]);
     });
 
     it("returns B1 if the cart contains P1 and P2", function () {
         const result = selectProducts("P1", "P2");
 
-        expect(result).toBe("B1");
+        expect(result).toStrictEqual(["B1"]);
     });
 
     it("returns B1 if the cart contains P2 and P1", function () {
         const result = selectProducts("P2", "P1");
 
-        expect(result).toBe("B1");
+        expect(result).toStrictEqual(["B1"]);
     });
 });
