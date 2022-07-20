527bdb0969e293641a01fcb31349d1d4f446a19c
Florian Barndt <florian.barndt@teufel.de>
2022-07-20 14:26:03 +0200

    Make P1,P2 -> B1 succeed

--- tdd/bundle-configurator/javascript/src/bundle-configurator.js
+++ tdd/bundle-configurator/javascript/src/bundle-configurator.js
@@ -1,5 +1,7 @@
-const selectProducts = (productNames) => {
-    return "P1";
+const selectProducts = (...productNames) => {
+    if (productNames.length === 1) return "P1";
+
+    return "B1";
 };
 
 module.exports = {selectProducts};
--- tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
+++ tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
@@ -1,9 +1,15 @@
 const {selectProducts} = require('../src/bundle-configurator');
 
 describe("Bundle configurator", function () {
-    it("should return the product if there's only one", function () {
+    it("returns the product if there's only one", function () {
         const result = selectProducts("P1");
 
         expect(result).toBe("P1");
+    });
+
+    it("returns B1 if the cart contains P1 and P2", function () {
+        const result = selectProducts("P1", "P2");
+
+        expect(result).toBe("B1");
     });
 });
