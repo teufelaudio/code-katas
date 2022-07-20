69d57b45177fe0cff248129f84b9abaf32cf8942
Florian Barndt <florian.barndt@teufel.de>
2022-07-20 14:21:31 +0200

    Rename first test

--- tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
+++ tdd/bundle-configurator/javascript/test/bundle-configurator.test.js
@@ -1,7 +1,7 @@
 const {selectProducts} = require('../src/bundle-configurator');
 
-describe("Bundle Configurator", function () {
-    it("select product P1", function () {
+describe("Bundle configurator", function () {
+    it("should return the product if there's only one", function () {
         const result = selectProducts("P1");
 
         expect(result).toBe("P1");
