c1780e3d1ef1c6a8451f27bf0d6faa3484beee1b
Florian Barndt <florian.barndt@teufel.de>
2022-10-20 14:16:07 +0200

    refactor code execution order in evaluateGuess

--- tdd/guess-random-number/javascript/src/kata.js
+++ tdd/guess-random-number/javascript/src/kata.js
@@ -1,14 +1,15 @@
 let randomNumber = 5;
 
 const evaluateGuess = (guessedNumber) => {
-  if (guessedNumber === randomNumber) {
-    return "number is correct";
-  }
   if (guessedNumber > randomNumber) {
     return "number too high";
   }
 
-  return "number too low";
+  if (guessedNumber < randomNumber) {
+    return "number too low";
+  }
+  
+  return "number is correct";
 };
 
 module.exports = { evaluateGuess };
