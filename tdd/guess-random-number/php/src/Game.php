0547da7f2fdaa419d9a5e12c7426ed25e761f644
Heiko Walz <heiko.walz@teufel.de>
2022-10-20 14:27:04 +0200

    Add tests for guessing it correct at last attempt.

--- tdd/guess-random-number/php/src/Game.php
+++ tdd/guess-random-number/php/src/Game.php
@@ -7,8 +7,13 @@ final class Game
     public const GUESS_IS_TOO_LOW = -1;
     public const GUESS_IS_CORRECT = 0;
     public const GUESS_IS_TOO_HIGH = 1;
+    public const GUESS_TOO_MANY_GUESSES = 42;
+
+    public const MAX_ATTEMPTS = 3;
 
     private int $number;
+
+    private int $attempts = 0;
 
     public function __construct(int $number)
     {
@@ -17,6 +22,18 @@ final class Game
 
     public function guess(int $guessNumber): int
     {
+        $this->attempts++;
+
+        if ($this->attempts > self::MAX_ATTEMPTS) {
+            return self::GUESS_TOO_MANY_GUESSES;
+        }
+        if ($this->attempts === self::MAX_ATTEMPTS) {
+            $correct = ($guessNumber <=> $this->number);
+            return ($correct === self::GUESS_IS_CORRECT)
+                ? self::GUESS_IS_CORRECT
+                : self::GUESS_TOO_MANY_GUESSES;
+        }
+
         return ($guessNumber <=> $this->number);
     }
 }
--- tdd/guess-random-number/php/tests/Unit/TheTest.php
+++ tdd/guess-random-number/php/tests/Unit/TheTest.php
@@ -48,4 +48,16 @@ final class TheTest extends TestCase
 
         self::assertEquals(Game::GUESS_IS_CORRECT, $answer);
     }
+
+    public function test_guess_the_incorrect_number_at_third_attempt(): void
+    {
+        $randomNumber = 50;
+
+        $theClass = new Game($randomNumber);
+        $answer = $theClass->guess(90);
+        $answer = $theClass->guess(40);
+        $answer = $theClass->guess(1);
+
+        self::assertEquals(Game::GUESS_TOO_MANY_GUESSES, $answer);
+    }
 }
