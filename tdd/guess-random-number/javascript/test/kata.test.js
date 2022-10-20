8ed82e5b153b95f8cec3f160dce851ea4a0dc5e0
Jose Maria Valera Reales <jose.valera@teufel.de>
2022-10-20 14:38:44 +0200

    refactor tests

--- tdd/guess-random-number/javascript/test/kata.test.js
+++ tdd/guess-random-number/javascript/test/kata.test.js
@@ -1,11 +1,10 @@
 const {Game} = require('../src/kata');
 
-describe("Guessing 5", function () {
-    it("leads to winning the game", function () {
-        let predefinedNumber = 5;
-        const result = new Game(predefinedNumber).evaluateGuess(5);
-
-        expect(result).toBe("number is correct");
+describe("_", function () {
+    it("The player wins when the input number matches the random number", function () {
+        expect(new Game(0).evaluateGuess(0)).toBe("number is correct");
+        expect(new Game(1).evaluateGuess(1)).toBe("number is correct");
+        expect(new Game(5).evaluateGuess(5)).toBe("number is correct");
     });
 });
 
