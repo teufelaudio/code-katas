e6c98bdc54e4dfad0c59b72c658a691999068655
Chemaclass <chemaclass@outlook.es>
2022-11-25 09:20:10 +0100

    Extend multiple iterations for Password Validator TDD kata

--- legacy/gilded-rose-refactoring/php/tests/GildedRoseTest.php
+++ legacy/gilded-rose-refactoring/php/tests/GildedRoseTest.php
@@ -12,7 +12,7 @@ class GildedRoseTest extends TestCase
 {
     public function testFoo(): void
     {
-        $items = [new Item('foo', 0, 0)];
+        $items = [new Item('fixme', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
         $this->assertSame('fixme', $items[0]->name);
