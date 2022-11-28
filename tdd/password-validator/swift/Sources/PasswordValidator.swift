f3f807dd0aaf01a3c249595c1da07498fe30b75b
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2022-11-28 14:01:05 +0100

    Check password length

--- tdd/password-validator/swift/Sources/PasswordValidator.swift
+++ tdd/password-validator/swift/Sources/PasswordValidator.swift
@@ -1,5 +1,11 @@
-public class PasswordValidator {
-    public func changeMe() -> Bool {
-        true
+public struct PasswordValidator {
+    public let password: String
+
+    internal init(for password: String) {
+        self.password = password
+    }
+
+    public func isValid() -> Bool {
+        password.count > 8
     }
 }
--- tdd/password-validator/swift/Tests/PasswordValidatorTest.swift
+++ tdd/password-validator/swift/Tests/PasswordValidatorTest.swift
@@ -2,8 +2,20 @@ import XCTest
 @testable import PasswordValidator
 
 final class PasswordValidatorTest: XCTestCase {
-    func testChangeMe() throws {
-        let validator = PasswordValidator()
-        XCTAssertTrue(validator.changeMe())
+
+    func testAtLeastEightCharactersFails() {
+        // given
+        let sut = PasswordValidator(for: "1234567")
+
+        // then
+        XCTAssertFalse(sut.isValid())
+    }
+
+    func testAtLeastEightCharactersSuccess() {
+        // given
+        let sut = PasswordValidator(for: "12345678")
+
+        // then
+        XCTAssertTrue(sut.isValid())
     }
 }
