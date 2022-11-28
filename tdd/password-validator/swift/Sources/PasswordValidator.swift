bc30ae67f7b3b5059bf1d5f0ca0e2185bc080334
Oguz Yuksel <oguz.yueksel@teufel.de>
2022-11-28 14:12:40 +0100

    add capital letter requirement

--- tdd/password-validator/swift/Sources/PasswordValidator.swift
+++ tdd/password-validator/swift/Sources/PasswordValidator.swift
@@ -1,11 +1,19 @@
+import Foundation
+
 public struct PasswordValidator {
     public let password: String
-
+    
     internal init(for password: String) {
         self.password = password
     }
-
+    
     public func isValid() -> Bool {
-        password.count > 8
+        let isCountValid = password.count > 8
+        var isUppercased = false
+        password.forEach { char in
+            if char.isUppercase { isUppercased = true }
+        }
+        
+        return isCountValid && isUppercased
     }
 }
--- tdd/password-validator/swift/Tests/PasswordValidatorTest.swift
+++ tdd/password-validator/swift/Tests/PasswordValidatorTest.swift
@@ -5,7 +5,7 @@ final class PasswordValidatorTest: XCTestCase {
 
     func testAtLeastEightCharactersFails() {
         // given
-        let sut = PasswordValidator(for: "1234567")
+        let sut = PasswordValidator(for: "1234567A")
 
         // then
         XCTAssertFalse(sut.isValid())
@@ -13,7 +13,23 @@ final class PasswordValidatorTest: XCTestCase {
 
     func testAtLeastEightCharactersSuccess() {
         // given
-        let sut = PasswordValidator(for: "12345678")
+        let sut = PasswordValidator(for: "12345678A")
+
+        // then
+        XCTAssertTrue(sut.isValid())
+    }
+    
+    func testContainsACapitalLetterFails() {
+        // given
+        let sut = PasswordValidator(for: "12345678a")
+
+        // then
+        XCTAssertFalse(sut.isValid())
+    }
+    
+    func testContainsACapitalLetterSuccess() {
+        // given
+        let sut = PasswordValidator(for: "12345678A")
 
         // then
         XCTAssertTrue(sut.isValid())
