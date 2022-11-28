a30a797e6ee7c7abdc868467d2411fb3770fd138
mark.nolan <mark.nolan@teufel.de>
2022-11-28 17:19:19 +0100

    kotlin implementation with decorator pattern

--- tdd/password-validator/kotlin/src/main/kotlin/PasswordValidator.kt
+++ tdd/password-validator/kotlin/src/main/kotlin/PasswordValidator.kt
@@ -1,5 +1,102 @@
-class PasswordValidator {
-    fun changeMe(): Boolean {
-        return true
+interface Validator {
+    fun validate(string: String): Boolean
+}
+
+class PasswordValidator : Validator {
+
+    private val passwordValidator: Validator =
+        LengthValidator(
+            MIN_PASSWORD_LENGTH,
+            ContainsUpperCaseValidator(
+                ContainsLowerCaseValidator(
+                    ContainsDigitValidator(
+                        ContainsSpecialCharacterValidator(
+                            DEFAULT_SPECIAL_CHARACTERS,
+                            TerminalValidator()
+                        )
+                    )
+                )
+            )
+        )
+
+    override fun validate(string: String) : Boolean = passwordValidator.validate(string)
+
+    companion object {
+        const val MIN_PASSWORD_LENGTH = 8
+        val DEFAULT_SPECIAL_CHARACTERS = setOf('_')
+    }
+
+}
+
+class LengthValidator(
+    private val minLength: Int,
+    private val validator: Validator
+) : Validator {
+    override fun validate(string: String): Boolean {
+        return if (string.length >= minLength) {
+            validator.validate(string)
+        } else {
+            println("Invalid Password Length")
+            false
+        }
     }
 }
+
+class ContainsUpperCaseValidator(
+    private val validator: Validator
+) : Validator {
+    override fun validate(string: String): Boolean {
+        return if (string.any { it.isUpperCase() }) {
+            validator.validate(string)
+        } else {
+            println("No upper case letter present - 'A-Z'")
+            false
+        }
+    }
+}
+
+class ContainsLowerCaseValidator(
+    private val validator: Validator
+) : Validator {
+    override fun validate(string: String): Boolean {
+        return if (string.any { it.isLowerCase() }) {
+            validator.validate(string)
+        } else {
+            println("No lower case letter present - 'a-z'")
+            false
+        }
+    }
+}
+
+class ContainsDigitValidator(
+    private val validator: Validator
+    ) : Validator {
+    override fun validate(string: String): Boolean {
+        return if (string.any { it.isDigit() }) {
+            validator.validate(string)
+        } else {
+            println("No digit present - '0-9'")
+            false
+        }
+    }
+
+}
+
+class ContainsSpecialCharacterValidator(
+    val charSet: Set<Char>,
+    val validator: Validator
+    ) : Validator {
+    override fun validate(string: String): Boolean {
+        return if (string.any { charSet.contains(it) } ) {
+            validator.validate(string)
+        } else {
+            println("No Special Character present - '$charSet'")
+            false
+        }
+    }
+}
+
+class TerminalValidator: Validator {
+    override fun validate(string: String): Boolean = true
+}
+
--- tdd/password-validator/kotlin/src/test/kotlin/PasswordValidatorTest.kt
+++ tdd/password-validator/kotlin/src/test/kotlin/PasswordValidatorTest.kt
@@ -1,11 +1,69 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 internal class PasswordValidatorTest {
 
     @Test
-    fun testChangeMe() {
-        val validator = PasswordValidator()
+    fun hasMinLength() {
+        val lengthValidator = LengthValidator(
+            MIN_PASSWORD_LENGTH,
+            TerminalValidator()
+        )
 
-        assert(validator.changeMe())
+        assertFalse(lengthValidator.validate("abc"))
     }
+
+    @Test
+    fun doesNotContainCapitalLetter() {
+        val containsUpperCaseValidator =
+            ContainsUpperCaseValidator(
+                TerminalValidator()
+            )
+
+        assertFalse(containsUpperCaseValidator.validate("abcdefghijk"))
+    }
+
+    @Test
+    fun doesNotContainLowercaseLetter() {
+        val containsLowerCaseValidator =
+            ContainsLowerCaseValidator(
+                TerminalValidator()
+            )
+
+        assertFalse(containsLowerCaseValidator.validate("AAAAAAAAAA"))
+    }
+
+    @Test
+    fun doesNotContainDigit() {
+        val containsDigitValidator =
+            ContainsDigitValidator(
+                TerminalValidator()
+            )
+
+        assertFalse(containsDigitValidator.validate("abcdeFGhijk"))
+    }
+
+    @Test
+    fun doesNotContainUnderscore() {
+        val containsSpecialCharacterValidator =
+            ContainsSpecialCharacterValidator(
+                DEFAULT_SPECIAL_CHARACTERS,
+                TerminalValidator()
+            )
+        assertFalse(containsSpecialCharacterValidator.validate("abcdeFGhij9"))
+    }
+
+    @Test
+    fun acceptsValidPassword() {
+        val passwordValidator = PasswordValidator()
+        assertTrue(passwordValidator.validate(VALID_PASSWORD))
+    }
+
+    companion object {
+        const val VALID_PASSWORD = "Abd3fg_i"
+        const val MIN_PASSWORD_LENGTH = 8
+        val DEFAULT_SPECIAL_CHARACTERS = setOf('_')
+    }
+
 }
