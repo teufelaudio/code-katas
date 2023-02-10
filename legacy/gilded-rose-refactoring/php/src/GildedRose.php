3fb48048680c2b15374410f7af82211402fc9d25
Dominik Marx <dominik.marx@teufel.de>
2023-02-10 14:15:56 +0100

    extract decrease and increase

--- legacy/gilded-rose-refactoring/php/src/GildedRose.php
+++ legacy/gilded-rose-refactoring/php/src/GildedRose.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace GildedRose;
 
@@ -10,6 +10,7 @@ final class GildedRose
     const ITEM_NAME_AGED_BRIE = 'Aged Brie';
     const SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
     const BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = 'Backstage passes to a TAFKAL80ETC concert';
+    const MAX_ITEM_QUALITY = 50;
 
     /**
      * @param Item[] $items
@@ -28,25 +29,19 @@ final class GildedRose
 
     public function updateItemQuality(Item $item): void
     {
-        if ($item->name !== self::ITEM_NAME_AGED_BRIE && $item->name !== self::BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT) {
-            if ($item->quality > 0) {
-                if ($item->name !== self::SULFURAS_HAND_OF_RAGNAROS) {
-                    --$item->quality;
-                }
+        if (!in_array($item->name, [self::ITEM_NAME_AGED_BRIE, self::BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT])) {
+            if ($item->name !== self::SULFURAS_HAND_OF_RAGNAROS) {
+                $this->decreaseQuality($item);
             }
         } else {
-            if ($item->quality < 50) {
+            if ($item->quality < self::MAX_ITEM_QUALITY) {
                 ++$item->quality;
                 if ($item->name === self::BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT) {
                     if ($item->sellIn < 11) {
-                        if ($item->quality < 50) {
-                            ++$item->quality;
-                        }
+                        $this->increaseQuality($item);
                     }
                     if ($item->sellIn < 6) {
-                        if ($item->quality < 50) {
-                            ++$item->quality;
-                        }
+                        $this->increaseQuality($item);
                     }
                 }
             }
@@ -59,17 +54,29 @@ final class GildedRose
         if ($item->sellIn < 0) {
             if ($item->name !== self::ITEM_NAME_AGED_BRIE) {
                 if ($item->name !== self::BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT) {
-                    if (($item->quality > 0) && $item->name !== self::SULFURAS_HAND_OF_RAGNAROS) {
-                        --$item->quality;
+                    if ($item->name !== self::SULFURAS_HAND_OF_RAGNAROS) {
+                        $this->decreaseQuality($item);
                     }
                 } else {
                     $item->quality -= $item->quality;
                 }
             } else {
-                if ($item->quality < 50) {
-                    ++$item->quality;
-                }
+                $this->increaseQuality($item);
             }
+        }
+    }
+
+    public function increaseQuality(Item $item): void
+    {
+        if ($item->quality < self::MAX_ITEM_QUALITY) {
+            ++$item->quality;
+        }
+    }
+
+    public function decreaseQuality(Item $item): void
+    {
+        if ($item->quality > 0) {
+            --$item->quality;
         }
     }
 }
