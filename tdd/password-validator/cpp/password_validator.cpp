5b26c1dddd5336744d4653841a663ccd7367cd05
Tim Keszler <tim.keszler@teufel.de>
2023-03-23 16:36:33 +0100

    added underscore test, refactorings

--- tdd/password-validator/cpp/password_validator.cpp
+++ tdd/password-validator/cpp/password_validator.cpp
@@ -4,11 +4,9 @@
 #include <string>
 
 // do something to a string
-inline std::string do_to_string(std::string str, const auto& fn) {
+inline std::string do_to_string(std::string str, int (*fn)(int)) {
   std::transform(std::begin(str), std::end(str), std::begin(str),
-                 [&](const std::string::value_type &x) {
-                   return fn(x);
-                 });
+                 [&](const std::string::value_type &x) { return fn(x); });
   return str;
 }
 
@@ -16,13 +14,16 @@ bool validator(std::string input) {
   if (input.size() < 9)
     return false;
 
-  if (input == do_to_string(input, [](auto x){return std::tolower(x);}))
+  if (input == do_to_string(input, std::tolower))
     return false;
 
-  if (input == do_to_string(input, [](auto x){return std::toupper(x);}))
+  if (input == do_to_string(input, std::toupper))
     return false;
 
   if (std::find_if(input.begin(), input.end(), ::isdigit) == input.end())
+    return false;
+
+  if (input.find('_') == std::string::npos)
     return false;
 
   return true;
