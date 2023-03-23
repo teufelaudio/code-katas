e921daf21e0ae389ef67048a3c90038eaf4e6e6b
Tim Keszler <tim.keszler@teufel.de>
2023-03-23 16:02:39 +0100

    added capital test

--- tdd/password-validator/cpp/password_validator.cpp
+++ tdd/password-validator/cpp/password_validator.cpp
@@ -1,6 +1,21 @@
-#include <string>
+#include <algorithm>
 #include <iostream>
+#include <string>
+
+inline std::string to_lower(std::string str) {
+  std::transform(std::begin(str), std::end(str), std::begin(str),
+                 [](const std::string::value_type &x) {
+                   return std::tolower(x, std::locale());
+                 });
+  return str;
+}
 
 bool validator(std::string input) {
-    return input.size() > 8;
+  if (input.size() < 9)
+    return false;
+
+  if (input == to_lower(input))
+    return false;
+
+  return true;
 }
--- tdd/password-validator/cpp/password_validator_test.cpp
+++ tdd/password-validator/cpp/password_validator_test.cpp
@@ -1,12 +1,16 @@
-#include <string>
 #include <gtest/gtest.h>
+#include <string>
 
 bool validator(std::string input);
 
-TEST(LENGTHTEST, TEST8CHARS) {
-    EXPECT_EQ(validator("12345678"), false);
-}
+TEST(HAPPYPATH, HAPPYPATH) { EXPECT_EQ(validator("Aa9_12345"), true); }
 
-TEST(LENGTHTEST, TEST9CHARS) {
-    EXPECT_EQ(validator("123456789"), true);
-}
+TEST(LENGTHTEST, TEST8CHARS) { EXPECT_EQ(validator("Aa9_1234"), false); }
+
+TEST(CAPITALTEST, NOCAPITAL) { EXPECT_EQ(validator("aa9_12345"), false); }
+
+TEST(CAPITALTEST, NOLOWERCASE) { EXPECT_EQ(validator("AA9_12345"), false); }
+
+TEST(CAPITALTEST, NONUMBER) { EXPECT_EQ(validator("AAa_teest"), false); }
+
+TEST(CAPITALTEST, NOUNDERSCORE) { EXPECT_EQ(validator("AA9a12345"), false); }
