a546748912be960c2c3e13777f691c42c93abc48
Mark Nolan <mark.nolan@teufel.de>
2023-04-17 14:24:53 +0200

    treeShouldHaveTrunk

--- tdd/xmas-tree/kotlin/src/main/kotlin/XmasTree.kt
+++ tdd/xmas-tree/kotlin/src/main/kotlin/XmasTree.kt
@@ -1,3 +1,5 @@
 class XmasTree {
 
+    fun tree() = "|"
+
 }
--- tdd/xmas-tree/kotlin/src/test/kotlin/XmasTreeTest.kt
+++ tdd/xmas-tree/kotlin/src/test/kotlin/XmasTreeTest.kt
@@ -1,6 +1,16 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 internal class XmasTreeTest {
+
+    @Test
+    fun treeShouldHaveTrunk() {
+        val xmas = XmasTree()
+
+        val output : String = xmas.tree()
+
+        assertEquals('|', output.last())
+    }
 
 }
