832536eb139b424442bba3bf65faac7b9fb894e8
sebastianlarisch <sebastian.larisch@teufel.de>
2023-06-30 13:45:08 +0200

    init project, install phpunit

---
+++ legacy/import-users/tests/ImportUsersRunCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ImportUsersRunCommandTest extends TestCase
+{
+    public function test(): void
+    {
+
+    }
+}
