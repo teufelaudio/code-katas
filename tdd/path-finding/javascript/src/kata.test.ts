feedb3ee54ef27f135970d4758943ad66e241227
Rick Peacock <rick.peacock@teufel.de>
2024-01-12 14:05:43 +0000

    Add recursive path finder

--- tdd/path-finding/javascript/src/kata.test.ts
+++ tdd/path-finding/javascript/src/kata.test.ts
@@ -1,13 +1,39 @@
 import Pathfinder from './kata';
 
+const pathfinder = new Pathfinder();
+
 describe('Path finder', () => {
     describe('Step 1: Compass directions', () => {
         it('gives no directions if the destination matches the start point', () => {
-            const pathfinder = new Pathfinder();
-
-            expect(pathfinder.provideDirections(0,0,0,0)).toEqual("")
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 0, y:0})).toEqual("")
         });
 
-        // Add more examples for other behaviours
+        it('gives directions for one step north', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 0, y:1})).toEqual("N")
+        });
+
+        it('gives directions for two steps north', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 0, y:2})).toEqual("NN")
+        });
+
+        it('gives directions for three steps north', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 0, y:3})).toEqual("NNN")
+        });
+
+        it('gives directions for three steps south', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 0, y:-3})).toEqual("SSS")
+        });
+
+        it('gives directions for three steps east', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x: 3, y:0})).toEqual("EEE")
+        });
+
+        it('gives directions for three steps west', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x:-3, y:0})).toEqual("WWW")
+        });
+
+        it('gives directions for two steps west and four steps south', () => {
+            expect(pathfinder.provideDirections({x: 0, y:0}, {x:-2, y:-4})).toEqual("SSSSWW")
+        });
     });
 });
